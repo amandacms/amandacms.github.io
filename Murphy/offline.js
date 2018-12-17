5fc68690170b258e98f0435b17b962844f4e160a
amanda13 <amanda001.ac@gmail.com>
2018-12-16 22:09:43 -0200

    att

---
+++ Murphy/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1545005311,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/murphy-sheet0.png",
+		"images/play_bottom-sheet0.png",
+		"images/playwithme-sheet0.png",
+		"images/quebra_cabeça-sheet0.png",
+		"images/jogo_da_memoria-sheet0.png",
+		"images/fundologo.png",
+		"images/fundomenu.png",
+		"images/fundomododejogo.png",
+		"images/imagemquebracabeça-sheet0.png",
+		"images/fundojogodamemoria.png",
+		"images/card1-sheet0.png",
+		"images/card1-sheet1.png",
+		"images/card1-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
