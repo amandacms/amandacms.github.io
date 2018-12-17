12767e01759cf978d42872cf909725830fa178d3
amanda13 <amanda001.ac@gmail.com>
2018-12-17 00:04:35 -0200

    att

--- Murphy/offline.js
+++ Murphy/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1545005729,
+	"version": 1545012217,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -18,6 +18,7 @@
 		"images/card1-sheet0.png",
 		"images/card1-sheet1.png",
 		"images/card1-sheet2.png",
+		"images/configurações-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
