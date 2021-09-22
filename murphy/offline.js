e040be4d0535c976698c65d7124305c9c143abd5
amandacms <amanda001.ac@gmail.com>
2021-09-22 11:58:28 -0300

    at

---
+++ murphy/offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1632321968,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/logoteste-sheet0.png",
+		"images/chao-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
